6e57a093013b6300aab46a7eb9613637fde4b818
Leni Kadali <lenikmutungi@gmail.com>
2019-05-11 07:39:15 +0300

    ft: Refactor home page view to use template, add `functional_tests`
    Refactored home page view to use template, added the `functional_tests`
    module containing functional tests and also added `lists`
    to INSTALLED_APPS. Added template.

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,8 @@
-from django.http import HttpRequest
-from django.urls import resolve
 from django.test import TestCase
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_home_page_view(self):
-        url = resolve('/')
-        self.assertEqual(url.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        resp = home_page(request)
-        html = resp.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        resp = self.client.get('/')
+        self.assertTemplateUsed(resp, 'home.html')
