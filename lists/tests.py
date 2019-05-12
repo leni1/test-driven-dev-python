6a711e879ed3c2b7bda27281f3ab0d51564a8bff
Leni Kadali <lenikmutungi@gmail.com>
2019-05-12 21:53:40 +0300

    ft: First stage of writing view and template tests
    First stage of writing view and template tests

--- lists/tests.py
+++ lists/tests.py
@@ -6,3 +6,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         resp = self.client.get('/')
         self.assertTemplateUsed(resp, 'home.html')
+
+    def test_can_save_post_request(self):
+        resp = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', resp.content.decode())
+        self.assertTemplateUsed(resp, 'home.html')
