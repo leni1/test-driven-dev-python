347baa16023169c1ec3eff492b02cb459e31dff3
Leni Kadali <lenikmutungi@gmail.com>
2019-05-20 22:05:00 +0300

    ft: Adjust models so that an item is assoc with a list
    Modified models and views so that an item is associated
    with a list.

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -10,24 +10,34 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(resp, 'home.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(first_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -37,8 +47,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(resp, 'list.html')
 
     def test_display_all_items(self):
-        Item.objects.create(text='Item 1')
-        Item.objects.create(text='Item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='Item 1', list=list_)
+        Item.objects.create(text='Item 2', list=list_)
 
         resp = self.client.get('/lists/the-only-list-in-the-world/')
 
