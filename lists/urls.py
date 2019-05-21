ca487e7e747d95b5d5272aa08f92884ad9474b46
Leni Kadali <lenikmutungi@gmail.com>
2019-05-21 08:24:16 +0300

    ft: Add lists pattern to superlists url module
    Added the lists pattern to superlists url module
    and removed it from the lists url module.

--- lists/urls.py
+++ lists/urls.py
@@ -6,8 +6,8 @@ app_name = 'lists'
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    path('lists/<int:list_id>/', views.view_list,
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list,
          name='view_list'),
-    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
 ]
