d5a1e89e8c668d6507ea6fd2ea25661540fb5ebd
Leni Kadali <lenikmutungi@gmail.com>
2019-05-21 17:37:33 +0300

    ft: Fix templates and urls so that functional tests pass
    Fixed templates and urls so that functional tests pass

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from lists import views as list_views
+
 urlpatterns = [
+    path('', list_views.home_page, name='home'),
     path('lists/', include('lists.urls')),
     path('admin/', admin.site.urls),
 ]
